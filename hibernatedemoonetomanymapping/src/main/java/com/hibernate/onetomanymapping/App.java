0c713a291041fe0620db6168891e5ab4bdaf40ab
RahulParimella <rahulsunny2472000@gmail.com>
2023-11-15 12:56:23 +0530

    first commit

---
+++ hibernatedemoonetomanymapping/src/main/java/com/hibernate/onetomanymapping/App.java
@@ -0,0 +1,13 @@
+package com.hibernate.onetomanymapping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
